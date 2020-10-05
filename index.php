ba01f655ffdc1a440a9e235d446a4d6e616b7e84
Celebro01 <dangerveneta@gmail.com>
2020-10-05 14:07:28 -0300

    arquivos iniciais

---
+++ index.php
@@ -0,0 +1,59 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>App Lista Tarefas</title>
+
+		<link rel="stylesheet" href="css/estilo.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+        <style>
+      .card-login {
+        padding: 30px 0 0 0;
+        width: 350px;
+        margin: 0 auto;
+      }
+    </style>
+	</head>
+
+	<body>
+		<nav class="navbar navbar-light bg-light">
+			<div class="container">
+				<a class="navbar-brand" href="#">
+					<img src="img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+					App Lista Tarefas
+				</a>
+			</div>
+		</nav>
+
+        <div class="container">    
+      <div class="row">
+
+        <div class="card-login">
+          <div class="card">
+            <div class="card-header">
+              Login
+            </div>
+            <div class="card-body">
+              <form action="../scripts/valida_login.php" method="post">
+                <div class="form-group">
+                  <input type="email" class="form-control" placeholder="E-mail" name="email">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control" placeholder="Senha" name="senha"> 
+                </div>
+                <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
+              </form>
+            </div>
+          </div>
+        </div>
+    </div>
+        
+
+
+	</body>
+</html>
+
+
+
+
